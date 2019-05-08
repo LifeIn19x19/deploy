9db60e74ebfce76c88a4375f943d5ce79574e328
Adrian Petrescu <apetresc@gmail.com>
2019-05-08 00:48:31 -0400

    Add Apache server to deployment

---
+++ Dockerfile.php
@@ -0,0 +1,15 @@
+FROM php:5.6-apache
+
+RUN apt-get update && apt-get install -y \
+        libfreetype6-dev \
+        libjpeg62-turbo-dev \
+        libpng-dev \
+        imagemagick \
+    && docker-php-ext-install -j$(nproc) iconv \
+    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+    && docker-php-ext-install -j$(nproc) gd \
+    && docker-php-ext-install -j$(nproc) mysqli
+#    && docker-php-ext-install -j$(nproc) zlib \
+#    && docker-php-ext-install -j$(nproc) imagick
+
+RUN printf '[PHP]\ndate.timezone = "UTC"\n' > /usr/local/etc/php/conf.d/tzone.ini
